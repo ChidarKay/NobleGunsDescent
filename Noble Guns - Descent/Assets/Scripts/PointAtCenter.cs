45209ae695f741c2e581a70240f01072a3b67dab
Chidar_K_ <eerotuo@gmail.com>
2017-03-30 17:11:08 +0300

    Can't remember what I did

--- Noble Guns - Descent/Assets/Scripts/PointAtCenter.cs
+++ Noble Guns - Descent/Assets/Scripts/PointAtCenter.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class PointAtCenter : MonoBehaviour {
 
+    GameObject firstPersonCharacter;
+    Camera cameraMain;
+    public float z;
+
+    void Start() {
+        firstPersonCharacter = GameObject.Find("FirstPersonCharacter");
+        cameraMain = firstPersonCharacter.GetComponent<Camera>();
+    }
+
 	void Update () {
+        transform.forward = cameraMain.ScreenToWorldPoint(new Vector3((Screen.width / 2), (Screen.height / 2), z));
 	}
 }
